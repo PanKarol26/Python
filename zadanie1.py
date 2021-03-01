90f9d84ba8cff09a17dc4e5220c61c4fd0f39fab
PanKarol26 <58815409+PanKarol26@users.noreply.github.com>
2021-03-01 04:19:07 +0100

    Add files via upload

---
+++ zadanie1.py
@@ -0,0 +1,31 @@
+# Zadanie 1
+# zmienna1 = 2
+# zmienna2 = 3
+# zmienna3 = 4.94
+# zmienna4 = 43.93
+# zmienna5 = 'Sosna'
+# zmienna6 = 'Świerk'
+# print(zmienna1)
+# print(zmienna2)
+# print(zmienna3)
+# print(zmienna4)
+# print(zmienna5)
+# print(zmienna6)
+
+
+# Zadanie 2
+# a = int(input("Podaj a: "))
+# b = int(input("Podaj b: "))
+# dodawanie = a+b
+# odejmowanie = a-b
+# mnozenie = a*b
+# dzielenie = a/b
+# print(dodawanie)
+# print(odejmowanie)
+# print(mnozenie)
+# print(dzielenie)
+
+# Zadanie 3
+
+
+
