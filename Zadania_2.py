77342e78ae2ba3a5d17e72e0b1e267e857552105
PanKarol26 <58815409+PanKarol26@users.noreply.github.com>
2021-03-08 03:28:19 +0100

    Add files via upload

---
+++ Zadania_2.py
@@ -0,0 +1,84 @@
+#zadanie_1
+import sys
+#lista = ["hockey", "siatkówka", "koszykówka", "piłka ręczna"]
+#print(lista)
+#lista.reverse()
+#print("Sporty w odwróconym porządku:")
+#print(lista)
+#nielubiany_sport = input("Dodaj sport do listy: ")
+#lista.append(nielubiany_sport)
+#print(lista)
+
+#zadanie_2
+#slownik = {'np.':'na przykład', 'p.n.e':'przed naszą erą', 'ms':'Microsoft', 'mess':'Messenger'}
+#print(slownik['ms'])
+
+#zadanie_3
+#gry = {'Heroes':'Might and Magic Heroes VII', 'Mario':'Super Mario Bros', 'Rayman':'Rayman Origins'}
+#print("Ilość gier: "(len(gry)))
+
+#zadanie_4
+#zdanie = input("Podaj zdanie: ")
+#litera = input("Podaj literę: ")
+#ilosc = (zdanie.count(litera))
+#print("Litera",litera, "występuje w tym zdaniu", ilosc, "razy." )
+
+#zadanie_6
+#a = int(input("Podaj a: "))
+#b = int(input("Podaj b: "))
+#c = int(input("Podaj c: "))
+#if a>b:
+#    if a>c:
+#        print("Największa jest liczba a =", a)
+#if b>a:
+#    if b>c:
+#        print("Największa jest liczba b =", b)
+#if c>a:
+#    if c>b:
+#        print("Największa jest liczba c =", c)
+#if a==b:
+#    if a>c:
+#        print("Największą wartość przyjmują liczby: a, b =", a)
+#if b==c:
+#    if b>a:
+#        print("Największą wartość przyjmują liczby: b, c =", b)
+#if a==c:
+#    if a>b:
+#        print("Największą wartość przyjmują liczby: a, c =", c)
+#if a==b==c:
+#    print("Wszystkie liczby są sobie równe")
+
+#zadanie_7
+#lista = [55, 2, 4, 13, 2.930, 4.153, 9.99]
+#for i in lista:
+#    i = i**2
+#    print(i)
+
+#zadanie_8
+#lista = []
+#i = 0
+#while i<10:
+#    liczba = int(input("Podaj liczbę:"))
+#    if liczba%2==0:
+#        lista.append(liczba)
+#    i = i+1
+#print(lista)
+
+#zadanie_9
+#lista = [1, 2, 3, 4, 5]
+#i = 0
+#while i<5:
+#    if lista[i]%2==0:
+#        print("E")
+#        i = i+1
+#    if lista[i]%2!=0:
+#        print("EEEEEE")
+#        i = i+1
+
+#zadanie_10
+#import math
+#liczba = int(input("Podaj liczbę: "))
+#if liczba>=0:
+#    print(math.sqrt(liczba))
+#else:
+#    print("BŁĄD!")
